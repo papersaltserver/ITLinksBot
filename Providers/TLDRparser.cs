16120c0c4bb97f398d0f28a13e13c7e4d2b34447
papersaltserver <noconnect@gmail.com>
2020-12-22 21:54:59 +0100

    Implemented TLDR newsletter

---
+++ Providers/TLDRparser.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using ItLinksBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ItLinksBot.Providers
+{
+    public class TLDRparser : IParser
+    {
+        readonly Provider _tldrProvider;
+        readonly Uri baseUri = new Uri("https://www.tldrnewsletter.com/");
+        public TLDRparser(Provider provider)
+        {
+            _tldrProvider = provider;
+        }
+        public string FormatDigestPost(Digest digest)
+        {
+            return string.Format("<b>{0}</b>\n{1}", digest.DigestName, digest.DigestURL);
+        }
+
+        public string FormatLinkPost(Link link)
+        {
+            return string.Format("<strong>{0}</strong>\n\n{1}\n{2}", link.Title, link.Description, link.URL);
+        }
+
+        public void GetCurrentDigests(out List<Digest> digests, out List<Link> links)
+        {
+            digests = new List<Digest>();
+            links = new List<Link>();
+            HttpClient httpClient = new HttpClient();
+            var archiveContent = httpClient.GetAsync(_tldrProvider.DigestURL).Result;
+            var stringResult = archiveContent.Content.ReadAsStringAsync().Result;
+            var digestArchiveHtml = new HtmlDocument();
+            digestArchiveHtml.LoadHtml(stringResult);
+            var digestsInArchive = digestArchiveHtml.DocumentNode.SelectNodes("//div[@id='doc-container']//div[contains(@class, 'd-lg-none')]//a").Take(50);
+            foreach(var digestNode in digestsInArchive)
+            {
+                var digestUrl = new Uri(baseUri, digestNode.GetAttributeValue("href", "Not found"));
+                var currentDigest = new Digest
+                {
+                    DigestDay = DateTime.Parse(HttpUtility.HtmlDecode(digestNode.InnerText).Replace("Daily Update","")),
+                    DigestName = HttpUtility.HtmlDecode(digestNode.InnerText).Trim(),
+                    DigestDescription = "", //tldr doesn't have description for digest itself
+                    DigestURL = digestUrl.AbsoluteUri,
+                    Provider = _tldrProvider
+                };
+                digests.Add(currentDigest);
+                var digestContent = httpClient.GetAsync(digestUrl).Result;
+                var linksHtml = new HtmlDocument();
+                linksHtml.LoadHtml(digestContent.Content.ReadAsStringAsync().Result);
+                var linksInDigest = linksHtml.DocumentNode.SelectNodes("//tr[@eo-body]//table[@eo-block='code']");
+                foreach (var link in linksInDigest)
+                {
+                    var titleNode = link.SelectSingleNode(".//div/span/a");
+                    var title = titleNode.InnerText;
+                    var href = titleNode.GetAttributeValue("href", "Not found");
+                    var description = link.SelectSingleNode(".//div/span/span").InnerText;
+                    links.Add(new Link
+                    {
+                        URL = href,
+                        Title = title,
+                        Description = description,
+                        Digest = currentDigest
+                    });
+                }
+            }
+        }
+    }
+}
