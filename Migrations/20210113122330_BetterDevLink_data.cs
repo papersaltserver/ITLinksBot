b9282c26df1b3174f38e0bbcba0d67c75d5e7382
papersaltserver <noconnect@gmail.com>
2021-01-13 16:03:20 +0100

    Implemented better dev link

---
+++ Migrations/20210113122330_BetterDevLink_data.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItLinksBot.Migrations
+{
+    public partial class BetterDevLink_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Providers",
+                columns: new[] {
+                    "ProviderID",
+                    "ProviderName",
+                    "DigestURL" },
+                values: new object[,] {
+                    {
+                        16,
+                        "Better Dev Link",
+                        "https://betterdev.link/issues"
+                    }
+                }
+                );
+            migrationBuilder.InsertData(
+                table: "TelegramChannels",
+                columns: new[] {
+                    "ChannelID",
+                    "ChannelName",
+                    "ProviderID"
+                },
+                values: new object[,]
+                {
+                    {
+                        16,
+                        "-1001288785667",
+                        16
+                    }
+                }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
