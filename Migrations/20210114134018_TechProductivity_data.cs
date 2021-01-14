cd7f8c962a8cf69d287733b3ac96d4613759ecac
papersaltserver <noconnect@gmail.com>
2021-01-14 21:03:11 +0100

    Added tech productivity

---
+++ Migrations/20210114134018_TechProductivity_data.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItLinksBot.Migrations
+{
+    public partial class TechProductivity_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Providers",
+                columns: new[] {
+                    "ProviderID",
+                    "ProviderName",
+                    "DigestURL" },
+                values: new object[,] {
+                    {
+                        19,
+                        "Tech Productivity",
+                        "https://techproductivity.co/archive/"
+                    }
+                }
+                );
+            migrationBuilder.InsertData(
+                table: "TelegramChannels",
+                columns: new[] {
+                    "ChannelID",
+                    "ChannelName",
+                    "ProviderID"
+                },
+                values: new object[,]
+                {
+                    {
+                        19,
+                        "-1001489422580",
+                        19
+                    }
+                }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Providers",
+                keyColumn: "ProviderID",
+                keyValue: 19
+                );
+            migrationBuilder.DeleteData(
+                table: "TelegramChannels",
+                keyColumn: "ChannelID",
+                keyValue: 19
+                );
+        }
+    }
+}
