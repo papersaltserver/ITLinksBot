e0dec540395c1487eb124f33e8e0765b6ccc3586
papersaltserver <noconnect@gmail.com>
2021-03-24 23:44:07 +0100

    Fixing tests running with dotnet build

---
+++ src/ItLinksBot-Test/UtilsTest.cs
@@ -0,0 +1,16 @@
+using ItLinksBot;
+using System;
+using Xunit;
+
+namespace ItLinksBot_Tests
+{
+    public class UtilsTest
+    {
+        [Theory]
+        [InlineData(471999600)]
+        public void UnixTimeStampToDateTimeTest(int timestamp)
+        {
+            Assert.Equal(new DateTime(1984,12,16), Utils.UnixTimeStampToDateTime(timestamp));
+        }
+    }
+}
