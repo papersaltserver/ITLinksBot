cfa51c108ea3bdcb060bc2742814c4a5c7be4f21
papersaltserver <noconnect@gmail.com>
2021-04-28 22:46:25 +0200

    Added DevOps weekly and small fixes to listing all the channels

---
+++ src/ItLinksBot/Migrations/20210427204509_DevopsWeekly_data.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItLinksBot.Migrations
+{
+    public partial class DevopsWeekly_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Providers",
+                columns: new[] {
+                    "ProviderID",
+                    "ProviderName",
+                    "DigestURL" },
+                values: new object[,] {
+                    {
+                        31,
+                        "Devops Weekly",
+                        "https://us2.campaign-archive.com/home/?u=b6635e37e35fa5eff0c2a947a&id=a63f24d068"
+                    }
+                }
+                );
+            migrationBuilder.InsertData(
+                table: "TelegramChannels",
+                columns: new[] {
+                    "ChannelID",
+                    "ChannelName",
+                    "ProviderID"
+                },
+                values: new object[,]
+                {
+                    {
+                        31,
+                        "-1001206101998",
+                        31
+                    }
+                }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Providers",
+                keyColumn: "ProviderID",
+                keyValue: 31
+                );
+            migrationBuilder.DeleteData(
+                table: "TelegramChannels",
+                keyColumn: "ChannelID",
+                keyValue: 31
+                );
+        }
+    }
+}
---
+++ src/ItLinksBot/Providers/DevopsWeeklyParser.cs
@@ -0,0 +1,146 @@
+﻿using HtmlAgilityPack;
+using ItLinksBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ItLinksBot.Providers
+{
+    class DevopsWeeklyParser: IParser
+    {
+        public string CurrentProvider => "Devops Weekly";
+        private readonly IContentGetter contentGetter;
+        private readonly IContentNormalizer contentNormalizer;
+        private readonly ITextSanitizer textSanitizer;
+        //readonly Uri baseUri = new("https://us2.campaign-archive.com");
+
+        public DevopsWeeklyParser(IContentGetter cg, IContentNormalizer cn, ITextSanitizer ts)
+        {
+            contentGetter = cg;
+            contentNormalizer = cn;
+            textSanitizer = ts;
+        }
+
+        public string FormatDigestPost(Digest digest)
+        {
+            return string.Format("<b>{0} - {1}</b>\n{2}\n{3}", digest.DigestName, digest.DigestDay.ToString("yyyy-MM-dd"), digest.DigestDescription, digest.DigestURL);
+        }
+
+        public string FormatLinkPost(Link link)
+        {
+            return string.Format("{0}\n{1}", link.Description, link.URL);
+        }
+
+        public List<Digest> GetCurrentDigests(Provider provider)
+        {
+            List<Digest> digests = new();
+            var stringResult = contentGetter.GetContent(provider.DigestURL);
+            var digestArchiveHtml = new HtmlDocument();
+            digestArchiveHtml.LoadHtml(stringResult);
+            var digestsInArchive = digestArchiveHtml.DocumentNode.SelectNodes("//li[contains(@class,'campaign')]").Take(5);
+            foreach (var digestNode in digestsInArchive)
+            {
+                var dateNode = digestNode.ChildNodes[0];
+                string dateText = dateNode.InnerText.Split('-')[0].Trim();
+                var digestDate = DateTime.Parse(dateText, new CultureInfo("en-US", false));
+                var hrefNode = digestNode.SelectSingleNode("./a");
+                var digestHref = hrefNode.GetAttributeValue("href", "Not found");
+                var digestName = hrefNode.InnerText.Trim();
+                //var digestUrl = new Uri(baseUri, digestHref);
+                var fullHref = Utils.UnshortenLink(digestHref);
+
+                var currentDigest = new Digest
+                {
+                    DigestDay = digestDate,
+                    DigestName = digestName,
+                    DigestDescription = "", //Devops Weekly doesn't provide one
+                    DigestURL = fullHref,
+                    Provider = provider
+                };
+                digests.Add(currentDigest);
+            }
+            return digests;
+        }
+
+        public Digest GetDigestDetails(Digest digest)
+        {
+            return digest;
+        }
+
+        public List<Link> GetDigestLinks(Digest digest)
+        {
+            List<Link> links = new();
+            var digestContent = contentGetter.GetContent(digest.DigestURL);
+            var linkMatches = Regex.Matches(digestContent, @"([^\<\>\r\n\=]+?)\<br\>\r\n\<br\>\r\n(.+?)\<br\>\r\n\<br\>\r\n\<br\>",RegexOptions.Singleline);
+            for(int i=1; i < linkMatches.Count; i++)
+            {
+                string title = "";
+                string description = linkMatches[i].Groups[1].Value;
+                string descriptionText = textSanitizer.Sanitize(description.Trim());
+                string hrefText = linkMatches[i].Groups[2].Value;
+                string[] hrefSplitArray = hrefText.Split(new string[] { "<br>" }, StringSplitOptions.None);
+                string href;
+                if(hrefSplitArray.Length == 1)
+                {
+                    href = Utils.UnshortenLink(hrefSplitArray[0].Trim());
+                }
+                else
+                {
+                    href = hrefSplitArray[hrefSplitArray.Length - 1];
+                    for(int j = 0; j < hrefSplitArray.Length - 1; j++)
+                    {
+                        descriptionText += $"\n{hrefSplitArray[j]}";
+                    }
+                }
+                links.Add(new Link
+                {
+                    URL = href,
+                    Title = title,
+                    Description = descriptionText,
+                    LinkOrder = i,
+                    Digest = digest
+                });
+            }
+            /*var linksHtml = new HtmlDocument();
+            linksHtml.LoadHtml(digestContent);
+            var linksInDigest = linksHtml.DocumentNode.SelectNodes("//*[@id='templateBody']//table//table//table//td[contains(@class,'mcnTextContent')][not(div)]");
+            for (int i = 0; i < linksInDigest.Count; i++)
+            {
+                HtmlNode link = linksInDigest[i];
+                var titleNode = link.SelectSingleNode("./ancestor::table[contains(@class,'mcnTextBlock')]/preceding-sibling::table[1]//div");
+                string title;
+                if (titleNode != null)
+                {
+                    title = titleNode.InnerText; //no titles
+                }
+                else
+                {
+                    title = "";
+                }
+                var href = digest.DigestURL + "#section-" + i;
+
+                //var contentNodes = link.SelectNodes("./table[not(contains(@class,'leading_item'))]");
+                string descriptionText;
+                var descriptionNode = HtmlNode.CreateNode("<div></div>");
+                descriptionNode.AppendChild(link);
+                descriptionNode = contentNormalizer.NormalizeDom(descriptionNode);
+                descriptionText = textSanitizer.Sanitize(descriptionNode.InnerHtml.Trim());
+
+                links.Add(new Link
+                {
+                    URL = href,
+                    Title = title,
+                    Description = descriptionText,
+                    LinkOrder = i,
+                    Digest = digest
+                });
+            }*/
+            return links;
+        }
+
+    }
+}
