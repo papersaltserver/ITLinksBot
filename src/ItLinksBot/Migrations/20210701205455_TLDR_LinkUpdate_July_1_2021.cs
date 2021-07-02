0ad817cc342e7ca459380982718a3b45cd678df7
papersaltserver <noconnect@gmail.com>
2021-07-02 11:02:55 +0200

    TLDR Link update 2021-06-30

---
+++ src/ItLinksBot/Migrations/20210701205455_TLDR_LinkUpdate_July_1_2021.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItLinksBot.Migrations
+{
+    public partial class TLDR_LinkUpdate_July_1_2021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderID",
+                keyValue: "3",
+                column: "DigestURL",
+                value: "https://tldr.tech/newsletter"
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderID",
+                keyValue: "3",
+                column: "DigestURL",
+                value: "https://tldr.tech/newsletter/archives"
+                );
+        }
+    }
+}
