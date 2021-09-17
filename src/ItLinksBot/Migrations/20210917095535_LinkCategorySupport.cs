b63836584e17b0ed9de92fe2ba9c09373760f2a1
papersaltserver <noconnect@gmail.com>
2021-09-17 11:59:48 +0200

    Added a new field to store category for each link entry

---
+++ src/ItLinksBot/Migrations/20210917095535_LinkCategorySupport.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItLinksBot.Migrations
+{
+    public partial class LinkCategorySupport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Links",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Links");
+        }
+    }
+}
