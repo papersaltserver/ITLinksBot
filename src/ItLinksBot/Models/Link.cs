324759158215c0c61364792edabe57b3add596f9
papersaltserver <noconnect@gmail.com>
2021-09-18 13:23:37 +0200

    added link category support

--- src/ItLinksBot/Models/Link.cs
+++ src/ItLinksBot/Models/Link.cs
@@ -12,6 +12,7 @@ namespace ItLinksBot.Models
         public string URL { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Category { get; set; }
         public int LinkOrder { get; set; }
         public ICollection<Media> Medias { get; set; }
         public Digest Digest { get; set; }
