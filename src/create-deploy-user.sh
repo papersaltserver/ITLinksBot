a39e40a5f9612bcc39792cc83047334609d3eadd
papersaltserver <noconnect@gmail.com>
2024-02-03 13:49:30 +0100

    feat: upgrade dependencies

--- src/create-deploy-user.sh
+++ src/create-deploy-user.sh
@@ -7,7 +7,7 @@ sudo chown -R deploy:deploy /home/deploy/
 sudo chown root:root /home/deploy
 sudo chmod 700 /home/deploy/.ssh
 sudo chmod 644 /home/deploy/.ssh/authorized_keys
-echo 'deploy  ALL=(ALL:ALL) NOPASSWD:/usr/bin/docker pull papersaltserver/itlinksbot\:latest,/usr/bin/docker rm -f oci-linksbot, /usr/bin/docker run -d --restart unless-stopped -v /home/ubuntu/net5.0/db/\:/app/db --env-file /home/ubuntu/net5.0/env.list  --name oci-linksbot papersaltserver/itlinksbot\:latest' | sudo tee --append /etc/sudoers
+echo 'deploy  ALL=(ALL:ALL) NOPASSWD:/usr/bin/docker pull papersaltserver/itlinksbot\:latest,/usr/bin/docker rm -f oci-linksbot, /usr/bin/docker run -d --restart unless-stopped -v /home/ubuntu/linksbot/db/\:/app/db --env-file /home/ubuntu/linksbot/env.list  --name oci-linksbot papersaltserver/itlinksbot\:latest' | sudo tee --append /etc/sudoers
 
 #put public key in authorized_keys
 #sudo vim /home/deploy/.ssh/authorized_keys
@@ -16,5 +16,5 @@ echo 'deploy  ALL=(ALL:ALL) NOPASSWD:/usr/bin/docker pull papersaltserver/itlink
 #LINKSBOT_BotApiKey=<API_KEY>
 #set log level to appropriate level in env file
 #LINKSBOT_Serilog__MinimumLevel=Debug
-#vim ~/net5.0/env.list
+#vim ~/linksbot/env.list
 
