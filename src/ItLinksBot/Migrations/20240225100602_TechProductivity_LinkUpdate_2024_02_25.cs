79b796b04e6ad944027a857797c9d26475fc0c5b
papersaltserver <noconnect@gmail.com>
2024-02-25 11:08:13 +0100

    fix: tech productivity link

---
+++ src/ItLinksBot/Migrations/20240225100602_TechProductivity_LinkUpdate_2024_02_25.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItLinksBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class TechProductivity_LinkUpdate_2024_02_25 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderID",
+                keyValue: "19",
+                column: "DigestURL",
+                value: "https://us5.campaign-archive.com/home/?u=ea228d7061e8bbfa8639666ad&id=aa93122b21"
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderID",
+                keyValue: "19",
+                column: "DigestURL",
+                value: "https://us20.campaign-archive.com/home/?u=88eb3ff0c5a479cf74013ef57&id=362b1686a3"
+                );
+        }
+    }
+}
