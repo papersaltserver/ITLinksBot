2989c22bda551e67800d817ea986321067a14b49
Dmitrii Trukhanov <dmitrii.trukhanov@nexontis.com>
2025-07-22 14:27:12 +0200

    fix messed links

---
+++ src/ItLinksBot/Migrations/20250722122209_CSharpDigest_ProgDig_Fix.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItLinksBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class CSharpDigest_ProgDig_Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                            table: "Providers",
+                            keyColumn: "ProviderID",
+                            keyValue: "9",
+                            column: "DigestURL",
+                            value: "https://programmingdigest.net/newsletters"
+                            );
+            migrationBuilder.UpdateData(
+                            table: "Providers",
+                            keyColumn: "ProviderID",
+                            keyValue: "10",
+                            column: "DigestURL",
+                            value: "https://csharpdigest.net/newsletters"
+                            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
