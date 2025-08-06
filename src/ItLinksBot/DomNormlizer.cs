a686925b730ef6bdd6f0b6214aa21883549f283a
Dmitrii Trukhanov <dmitrii.trukhanov@nexontis.com>
2025-08-06 12:35:08 +0200

    fix: allow line break on <br>

--- src/ItLinksBot/DomNormlizer.cs
+++ src/ItLinksBot/DomNormlizer.cs
@@ -6,7 +6,7 @@ namespace ItLinksBot
 {
     public class DomNormlizer : IContentNormalizer
     {
-        private string[] acceptableTags = new string[] { "strong", "em", "u", "b", "i", "a", "ins", "s", "strike", "del", "code", "pre" };
+        private string[] acceptableTags = new string[] { "strong", "em", "u", "b", "i", "a", "ins", "s", "strike", "del", "code", "pre", "br" };
         private string[] nodesProhibited = new string[] { "style", "script" };
         private string[] nodesNewLines = new string[] { "div", "p", "h1", "h2", "h3", "h4", "li", "td" };
         public HtmlNode NormalizeDom(HtmlNode originalNode, string[] accptableTags, string[] nodesProhibited, string[] nodesNewLines)
